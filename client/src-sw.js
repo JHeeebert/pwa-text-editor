457057ed9bf5d104859909ceb5fa1e4032bd51bb
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-23 01:24:42 -0400

    added src-sw.js logic

--- client/src-sw.js
+++ client/src-sw.js
@@ -25,6 +25,21 @@ warmStrategyCache({
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => 
+// Create a callback function that will filter out any requests we want to cache
+({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new StaleWhileRevalidate({
+  // Name of the cache Storage.
+  cacheName: 'assets-cache',
+  plugins: [
+  // Plugin will cache responses for 30 days.
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  // Plugin will delete old responses to conserve disk space.
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+}));
